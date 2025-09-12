c514843186b00ef660e744bcaafcb9329d81d141
Boki Todorcic <boki.todorcic@softwire.com>
2025-09-12 11:42:12 +0100

    Created test suit for only multiples of 5

---
+++ tests/multiplesOfFiveFizzBuzz.test.js
@@ -0,0 +1,27 @@
+const fizzbuzz = require("../src/fizzbuzz.js");
+
+// Testing results for only Buzz - I.e. only multiples of 5
+
+test("return Buzz for position 5", () => {
+  let position = 5;
+  const resultArray = fizzbuzz(position);
+  expect(resultArray[position - 1]).toEqual("Buzz");
+});
+
+test("return Buzz for position 10", () => {
+  let position = 10;
+  const resultArray = fizzbuzz(position);
+  expect(resultArray[position - 1]).toEqual("Buzz");
+});
+
+test("return Buzz for position 25", () => {
+  let position = 25;
+  const resultArray = fizzbuzz(position);
+  expect(resultArray[position - 1]).toEqual("Buzz");
+});
+
+test("return Buzz for position 85", () => {
+  let position = 85;
+  const resultArray = fizzbuzz(position);
+  expect(resultArray[position - 1]).toEqual("Buzz");
+});
